a6f8f61abde04f5c0e7f7d199854fa2fc320eb85
Anderson Silva <andeerson0017@gmail.com>
2020-11-13 19:14:56 -0300

    :white_check_mark: test: criando primeiro teste

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Simula a DOM
   testEnvironment: 'jsdom',
   // Ignorar essas pastas
-  testPathsIgnorePatterns: ['/node_modules', '/.next'],
+  testPathIgnorePatterns: ['/node_modules', '/.next'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main>
+      <h2>Anderson Silva</h2>
+    </main>
+  )
+}
+
+export default Main
