f82061873a42c7b91d6c9f37af4a9bf0974d1ba6
Anderson Silva <andeerson0017@gmail.com>
2020-11-14 18:33:07 -0300

    :white_check_mark: test: criando teste para componente Main

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,10 +4,25 @@ import Main from '.'
 describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
-    expect(
-      screen.getByRole('heading', { name: /anderson silva/i })
-    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /nextjs/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the styles correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b',
+      color: '#fff',
+      width: '100%',
+      height: '100%',
+      padding: '3rem',
+      display: 'flex',
+      'flex-direction': 'column',
+      'text-align': 'center',
+      'align-items': 'center',
+      'justify-content': 'center'
+    })
   })
 })
