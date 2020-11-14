827e6428d7b5e13187af76fe83aabe48cc9e9468
Anderson Silva <andeerson0017@gmail.com>
2020-11-14 18:31:14 -0300

    :recycle: refactor: componente Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 export default function Main() {
   return (
-    <main>
-      <h2>Anderson Silva</h2>
-    </main>
+    <S.Container>
+      <S.Logo
+        src="/images/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Title>Nextjs</S.Title>
+      <S.Description>
+        Typescript, Reactjs, Nextjs e Styled components
+      </S.Description>
+      <S.Illustration
+        src="/images/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código"
+      />
+    </S.Container>
   )
 }
